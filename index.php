717679ac69b373da09587fd045a38f4e446dc92e
majidsofyan22 <yassineelyousfi.60@gmail.com>
2025-09-12 08:42:12 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+$connect_database = mysqli_connect( $host, $username, $password, $dbname );
+
+if(mysqli_connect_errno()){
+
+    echo "not connected - غير متصل";
+    exit();
+}
+
+else{
+    echo "connected - متصل";
+}
+
+
+
+
+?>
